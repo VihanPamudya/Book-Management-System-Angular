5c6b34db3cc0305f5cbcef544a484b080729450a
VihanPamudya <vihanpamudya12@gmail.com>
2022-12-31 00:57:16 +0530

    changes

--- Book-Management-System-Frontend/src/app/app.module.ts
+++ Book-Management-System-Frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { AllBookListComponent } from './all-book-list/all-book-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    AllBookListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
