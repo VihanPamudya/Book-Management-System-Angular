6b39acb7a2ee195d47f7455648ccfdf4336fd7ae
VihanPamudya <vihanpamudya12@gmail.com>
2023-01-02 15:37:57 +0530

    crud updated

--- Book-Management-System-Frontend/src/app/components/book/book-update/book-update.component.ts
+++ Book-Management-System-Frontend/src/app/components/book/book-update/book-update.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-book-update',
@@ -7,5 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookUpdateComponent implements OnInit {
 
-  
+  id!: number;
+  isUpdating = false;
+  book: any;
+  constructor(private bookService: BookService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.id = JSON.parse(localStorage.getItem('id') || '{}');
+    this.bookService.getBook(this.id).subscribe({
+      next: (res) => {
+        this.book = res;
+        console.log(this.book.id);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+
+    });
+  }
+
+  onSubmit() {
+    console.log(this.book);
+    this.isUpdating = true;
+
+    this.bookService.update(this.id, this.book).subscribe({
+      next: (res) => {
+        if (res) {
+          this.isUpdating = false;
+          alert("Book Details Updated");
+          console.log('response: ' + res);
+          this.router.navigate(["/"]);
+        }
+      },
+      error: (err) => {
+        this.isUpdating = false;
+        console.log('Error: ' + err);
+        alert("Book Details not updated...!")
+      },
+    });
+
+
+  }
 }
